bd373c156e2a383f64d235e09b53d32a4a23835d
efreen martinez <genliger@gmail.com>
2020-10-01 18:18:18 -0500

    rename build to docs and commited

---
+++ docs/precache-manifest.ead29770d30a91a4657dbb31053b76c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4c07a460a939f1676dfc330c16f60af",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b63f4ab4f4ee27c36a2e",
+    "url": "/static/css/main.f47d73dd.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b63f4ab4f4ee27c36a2e",
+    "url": "/static/js/main.59c3aed6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
